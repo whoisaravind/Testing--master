01539cd9fa8a96a93e473f905095f15e5be6f1a3
whoisaravind <AravindLPU1#>
2022-03-26 10:25:51 +0530

    Link 4

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -129,7 +129,7 @@ const About = ({ data }) => {
           </SkillsContainer>
         </StyledContent>
         <StyledPic>
-          <StyledAvatarLink href={linkedin}>
+          <StyledAvatarLink href={}>
             <StyledAvatar fluid={avatar.childImageSharp.fluid} alt="Avatar" />
           </StyledAvatarLink>
         </StyledPic>
