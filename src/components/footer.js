d68c3a5c5b30bdf5d613e68ad0e92b2dfcb0cd12
whoisaravind <AravindLPU1#>
2022-03-26 13:04:41 +0530

    Footer Twitter

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,7 +102,7 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
+          href="https://twitter.com/whoisaravind/"
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>
